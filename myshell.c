c8c87bb2f2755882e778c379f01b5aa07ed7e6cf
qkd05852 <qkd05852@naver.com>
2016-12-04 02:58:40 +0900

    ShellProgram

--- myshell.c
+++ myshell.c
@@ -10,7 +10,9 @@ void background_process_sigfunc(int signo); // 백그라운드 처리
 int parsing(char * , char *[]); // 파싱
 void execute (char *[], int); // 파싱된 명령어 실행
 void pipe_execute(char *[], char *[]); // '|' 파이프
-
+void additional_parsing(char *, char*[], char *[]); //파이프, 리다이렉션을 위한 추가적인 파싱
+void normal_command_execute(char *[], int); // bash의 기본 명령어 실행
+void my_command_execute(char *[], int); // 내장명령어(my명령어) 실행
 
 // 명령어 함수
 void change_directory(char *[]);
@@ -94,7 +96,7 @@ void prompt(char buffer[])
 
 void stdin_redirection_execute(char *commandSet[], char *rear_command[])
 {
-	int fd, in; // fd = 파일 저장을 위한 파일 디스크립터 변수                             in = 표준입력을 복사, 저장하기위한 변수
+	int fd, in; // fd = 파일 저장을 위한 파일 디스크립터 변수                                in = 표준입력을 복사, 저장하기위한 변수
 	
 	in = dup(0); // dep(0)을 통해 in변수에 표준입력을 복사 저장
 	if((fd = open(rear_command[0], O_RDONLY, 0644)) < 0)
@@ -114,7 +116,7 @@ void stdin_redirection_execute(char *commandSet[], char *rear_command[])
 void stdout_redirection_execute(char *commandSet[], char *rear_command[])
 {
 	int fd, out; // fd = 파일 저장을 위한 파일 디스크립터 변서
-		        out = 표준출력을 복사, 저장하기위한 변수
+		     // out = 표준출력을 복사, 저장하기위한 변수
 
 	out = dup(1); // dep(1)을 통해 out변수에 표준출력을 복사 저장
 	if((fd = open(rear_command[0], O_CREAT|O_WRONLY|O_TRUNC, 0644)) < 0)
@@ -130,6 +132,8 @@ void stdout_redirection_execute(char *commandSet[], char *rear_command[])
 	execute(commandSet, 0);
 	close(1);
 	dup2(out, 1); // change to stdout
+}
+
 int parsing(char *buffer, char *commandSet[])
 {
 	char *temp_token = NULL; //자른 명령어를 임시적으로 저장하기위한 포인터
@@ -182,7 +186,7 @@ int parsing(char *buffer, char *commandSet[])
 
 void execute(char *commandSet[], int flag)
 {
-	if(strncmp(commandSet[0], "my", 2) == 0
+	if(strncmp(commandSet[0], "my", 2) == 0)
 	{
 		my_command_execute(commandSet, flag);
 	}
@@ -196,6 +200,151 @@ void execute(char *commandSet[], int flag)
 
 }
 
+void normal_command_execute(char *commandSet[], int flag)
+{
+	pid_t pid; //자식프로세스의 정보를 저장하기위한 변수
+	int status; //자식프로세스의 상태를 저장하기위한 변수
+	struct sigaction act; //백그라운드 환경을 위한 sigaction변수
+	act.sa_handler = background_process_sigfunc;
+	act.sa_flags = SA_RESTART;
+	sigaction(SIGCHLD, &act, NULL);
+
+	if((pid = fork()) < 0) // fork()를 통해 자식프로세스 생성
+	{
+		fprintf(stderr, "ERROR : Fork error ocurred\n");
+		exit(1);
+	}
+	else if (pid == 0)
+	{
+		if(execvp(commandSet[0], commandSet) < 0) // evecvp() 를 통해 bash에 있는 기본 명령어를 실행
+		{
+			fprintf(stderr, "ERROR : Exec error ocurred\n");
+			exit(1);
+		}
+	}
+	else
+	{
+		if(flag == 0) //백그라운드 실행이 아닐경우
+		{
+			if(waitpid(pid, &status, 0) < 0)
+			{
+				fprintf(stderr, "ERROR : waitpid() error ocurred\n");
+				exit(1);
+			}
+		}
+// 부모 프로세스가 waitpid()를 통해 실행이 끝난 자식프로세스를 기다렸다가 종료
+
+		else if(flag == 1) // 백그라운드 실행환경일 경우
+		{
+			printf("Background process pid : %d\n", pid);
+//백그라운드 프로세스 pid를 출력
+			waitpid(pid, &status, WNOHANG);
+//WNOHANG옵션으로 waitpid()를 실행, WNOHANG 옵션을 사용하면 block되지 않고 다른 작업 실행 가능
+		}
+	}
+}
+
+void my_command_execute(char *commandSet[], int flag)
+{
+	pid_t pid; // 자식프로세스의 정보를 저장하기위한 변수
+	int status; // 자식프로세스의 상태를 저장하기 위한 변수
+
+	if((pid = fork()) < 0) // fork()를 통해 자식프로세스 생성
+	{
+		fprintf(stderr, "ERROR : fork() error ocurred\n");
+		exit(1);
+	}
+	else if (pid == 0) // 자식프로세스에서 내장명령어(my명령어)를 확인하고 실행
+	{
+		if(strcmp(commandSet[0], "mypwd") == 0)
+		{
+			mypwd(commandSet);
+			exit(0);
+		}
+		else if(strcmp(commandSet[0], "myls") == 0)
+		{
+			myls(commandSet);
+			exit(0);
+		}
+		
+		else if(strcmp(commandSet[0], "mycmp") == 0)
+		{
+			mycmp(commandSet);
+			exit(0);
+		}
+
+		else if(strcmp(commandSet[0], "mycp") == 0)
+		{
+			mycp(commandSet);
+			exit(0);
+		}
+
+		else if(strcmp(commandSet[0], "mymv") == 0)
+		{
+			mymv(commandSet);
+			exit(0);
+		}
+
+		else if(strcmp(commandSet[0], "mymkdir") == 0)
+		{
+			mymkdir(commandSet);
+			exit(0);
+		}
+
+		else if(strcmp(commandSet[0], "myrmdir") == 0)
+		{
+			myrmdir(commandSet);
+			exit(0);
+		}
+
+		else if(strcmp(commandSet[0], "mycat") == 0)
+		{
+			mycat(commandSet);
+			exit(0);
+		}
+
+		else if(strcmp(commandSet[0], "myhead") == 0)
+		{
+			myhead(commandSet);
+			exit(0);
+		}
+
+		else if(strcmp(commandSet[0], "mytail") == 0)
+		{
+			mytail(commandSet);
+			exit(0);
+		}
+
+		else if(strcmp(commandSet[0], "myrm") == 0)
+		{
+			myrm(commandSet);
+			exit(0);
+		}
+		else
+		{
+			fprintf(stderr, "ERROR : My command is not correct\n");
+			exit(0);
+		}
+	}
+	else
+	{
+		if(flag == 0)
+		{
+			if(waitpid(pid, &status, 0) < 0)
+			{
+				fprintf(stderr, "ERROR : waitpid() error ocurred\n");
+				exit(1);
+			}
+		}
+		else if (flag == 1)
+		{
+			printf("Background process pid : %d\n", pid);
+			waitpid(pid, &status, WNOHANG);
+		}
+	}
+}
+
+		
 void pipe_execute(char *commandSet[], char *rear_command[])
 {
 	int fd[2]; // 파이프를 위한 배열
@@ -216,11 +365,23 @@ void pipe_execute(char *commandSet[], char *rear_command[])
 	dup2(out, 1);
 
 	close(0);
-	dup(fd[0], 0);
+	dup2(fd[0], 0);
 	close(fd[0]);
 	execute(rear_command, 0);
 	dup2(in, 0);
 }
+
+void additional_parsing(char buffer[], char *front_command[], char *rear_command[]) // 파이프와 리다이렉션을 위한 추가적 파싱
+{
+	char *buf1 = NULL;
+	char *buf2 = NULL;
+// 앞과 뒤 명령어를 나눈것을 임시 저장하기 위한 버퍼
+	
+	buf1 = strtok(buffer, "><|");
+	buf2 = strtok(NULL, "><|");
+	parsing(buf1, front_command);
+	parsing(buf2, rear_command);
+} // 파싱 과정을 거쳐 front_command와 rear_command에 명령어 저앙
 
 void background_process_sigfunc(int signo)
 {
